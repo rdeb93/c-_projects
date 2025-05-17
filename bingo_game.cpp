e0fcbc648d209cae01ec5cd9d099739d00b243b7
Ryan Debono <rdebono93@gmail.com>
2025-05-17 14:45:31 +0200

    first commit

---
+++ bingo_game.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <random>
+
+int bingo_card()
+{
+    // Define the number of tiles in the bingo card.
+    const int grid_width = 9;
+    const int grid_height = 3;
+
+    // Define the characters used to draw the bingo card.
+    const char horizontal = '-';
+    const char vertical = '|';
+    const char corner = '+';
+
+    // Set the width of each tile in the bingo card
+    const int tile_width = 3;
+
+    // Draw the top border
+    std::cout << corner;
+    for (int j=0; j<grid_width; j++){
+        for (int k=0; k<tile_width; k++){
+            std::cout << horizontal;
+        }
+    }
+    std::cout << corner;
+    std::cout << std::endl;
+    
+}
